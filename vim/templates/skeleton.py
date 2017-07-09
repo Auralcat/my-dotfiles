e655f0739bda91ba525cc049e2dd581eda115e6f
Auralcat <ashleyzt@hi2.in>
2017-07-08 23:48:38 -0300

    Fixed the Python templates for Vim

--- vim/templates/skeleton.py
+++ vim/templates/skeleton.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 
-"""Lorem Ipsum in Python3"""
--- vim/templates/skeleton_euler.py
+++ vim/templates/skeleton_euler.py
@@ -1,6 +1,11 @@
 """Insert problem header here."""
 
+from time import clock
+
 def main():
     # Code goes here
 
-
+start = clock()
+main()
+end = clock()
+print("Time elapsed: %s seconds." % (end - start))
