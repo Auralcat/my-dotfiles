7153dafc82c25d29dd7b8fce8694f278b671df1a
Auralcat <ashleyzt@hi2.in>
2017-11-21 15:49:38 -0200

    Upgrade magit-popup

--- .emacs.d/elpa/magit-popup-2.11.0/magit-popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; magit-popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/magit-popup-2.11.0/magit-popup-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/magit-popup-2.11.0/magit-popup-pkg.el"
-;;;;;;  "../../../../.emacs.d/elpa/magit-popup-2.11.0/magit-popup.el")
-;;;;;;  (23040 56384 413665 351000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-popup-autoloads.el ends here
