3ab368b11795485ca85ec30198e579d5f0223f42
Auralcat <ashleyzt@hi2.in>
2017-12-03 09:41:43 -0200

    Change face for previously scheduled items

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -400,6 +400,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-scheduled-previously ((t (:foreground "#778855"))))
  '(org-upcoming-deadline ((t (:foreground "#B1AA98")))))
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
