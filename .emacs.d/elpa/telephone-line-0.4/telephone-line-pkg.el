28d8789bd3a7e9762d2c0ff883a669b0b739a4a1
Auralcat <ashleyzt@hi2.in>
2017-12-25 18:10:50 -0200

    Switch powerline package for telephone-line

---
+++ .emacs.d/elpa/telephone-line-0.4/telephone-line-pkg.el
@@ -0,0 +1,10 @@
+(define-package "telephone-line" "0.4" "Rewrite of Powerline"
+  '((emacs "24.4")
+    (cl-lib "0.5")
+    (cl-generic "0.2")
+    (seq "1.8"))
+  :url "https://github.com/dbordak/telephone-line" :keywords
+  '("mode-line"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
