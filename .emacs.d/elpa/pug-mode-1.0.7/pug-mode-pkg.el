fa4def25c48868547ad7eb538362003694822247
Auralcat <ashleyzt@hi2.in>
2018-03-26 19:58:50 -0300

    Remove haml-mode and pug-mode

--- .emacs.d/elpa/pug-mode-1.0.7/pug-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pug-mode" "1.0.7" "Major mode for jade/pug template files" '((emacs "24.4") (cl-lib "0.5")) :commit "96718f802dad3acd5a3f770b1452b81e39f77d92" :url "https://github.com/hlissner/emacs-pug-mode" :keywords '("markup" "language" "jade" "pug"))
