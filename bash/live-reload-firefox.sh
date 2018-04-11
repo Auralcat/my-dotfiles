387add801ee2ee7a66752db8285d50de77f97d48
Auralcat <ashleyzt@hi2.in>
2018-04-11 10:54:03 -0300

    Create live-reload-firefox.sh

--- bash/live-reload-firefox.sh
+++ bash/live-reload-firefox.sh
@@ -8,11 +8,13 @@ firefoxWindow=$(xdotool search --name "Mozilla Firefox")
 emacsWindow=$(xdotool search --name "emacs24@")
 
 xdotool windowactivate $firefoxWindow
+# There's a delay in xdotool, hence this sleep
+sleep 0.5
 xdotool key F5
 
 # Talk to me
-notify-send -t 3000 "Viewing page, will return to Emacs in 10s"
-sleep 7
+notify-send -t 3000 "Viewing page, will return to Emacs in 6s"
+sleep 3
 notify-send -t 3000 "Returning to Emacs..."
 sleep 3
 
