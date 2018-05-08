6c56526032a0f1a1bf90f48aa2044ddfd07d6edc
Auralcat <ashleyzt@hi2.in>
2018-05-08 15:01:22 -0300

    Comment out default face setting in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -843,7 +843,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 4096)) (:foreground "#5f5f5f" :background "#fdfde7")) (((class color) (min-colors 256)) (:foreground "#5f5f5f" :background "#fdfde7")) (((class color) (min-colors 89)) (:foreground "#5f5f5f" :background "#fdfde7"))))
+ ;; '(default ((((class color) (min-colors 4096)) (:foreground "#5f5f5f" :background "#fdfde7")) (((class color) (min-colors 256)) (:foreground "#5f5f5f" :background "#fdfde7")) (((class color) (min-colors 89)) (:foreground "#5f5f5f" :background "#fdfde7"))))
  '(company-scrollbar-bg ((((class color) (min-colors 89)) (:background "#5f5f5f"))))
  '(company-scrollbar-fg ((((class color) (min-colors 89)) (:background "#9e9e9e"))))
  '(company-template-field ((((class color) (min-colors 89)) (:background "#ffffaf" :foreground "#626262"))))
