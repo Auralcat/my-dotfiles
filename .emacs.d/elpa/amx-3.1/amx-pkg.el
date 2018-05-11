20c4c3e4736aab2b437530c8fb1293f3a4359ae4
Auralcat <ashleyzt@hi2.in>
2018-05-11 20:06:36 -0300

    Install amx-mode and its dependencies

---
+++ .emacs.d/elpa/amx-3.1/amx-pkg.el
@@ -0,0 +1,1 @@
+(define-package "amx" "3.1" "Alternative M-x with extra features." '((emacs "24.4") (s "0")) :commit "0718391e11a6c4310ba7a8069a16c462b10ff4c4" :keywords '("convenience" "usability") :url "http://github.com/DarwinAwardWinner/amx/")
---
+++ .emacs.d/elpa/memoize-1.1/memoize-autoloads.el
@@ -0,0 +1,15 @@
+;;; memoize-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("memoize.el") (23286 8360 821685 780000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; memoize-autoloads.el ends here
