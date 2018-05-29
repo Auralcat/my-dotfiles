03312ed8980f4afb20f5e20aad36017f691cfbcd
Auralcat <ashleyzt@hi2.in>
2018-05-29 08:45:16 -0300

    Upgrade packages, remove org-beautify

--- .emacs.d/elpa/org-beautify-theme-0.3.1/org-beautify-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-beautify-theme" "0.3.1" "A sub-theme to make org-mode more beautiful." 'nil :commit "7b7a7cbd4f25f77e8bd81783f517b2b182220fd9" :keywords '("org" "theme"))
--- .emacs.d/elpa/simple-httpd-1.5.0/simple-httpd-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "simple-httpd" "1.5.0" "pure elisp HTTP server" '((cl-lib "0.3")))
