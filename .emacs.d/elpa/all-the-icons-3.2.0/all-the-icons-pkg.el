bf71964b36adf936e8fc4ff7ed8094f636bdb643
Auralcat <ashleyzt@hi2.in>
2018-06-02 13:24:40 -0300

    Install all-the-icons

---
+++ .emacs.d/elpa/all-the-icons-3.2.0/all-the-icons-pkg.el
@@ -0,0 +1,9 @@
+(define-package "all-the-icons" "3.2.0" "A library for inserting Developer icons"
+  '((emacs "24.3")
+    (memoize "1.0.1"))
+  :keywords
+  '("convenient" "lisp")
+  :url "https://github.com/domtronn/all-the-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
