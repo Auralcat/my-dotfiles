149bee6fa9994f87ad8cc3e6adafbbbc7826f5be
Auralcat <ashleyzt@hi2.in>
2018-06-08 01:35:26 -0300

    Upgrade packages, install cyberpunk-theme

--- .emacs.d/elpa/company-php-2.0.4/company-php-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; company-php-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "company-php" "company-php.el" (23237 38577
-;;;;;;  910821 988000))
-;;; Generated autoloads from company-php.el
-
-(autoload 'company-ac-php-backend "company-php" "\
-
-
-\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; company-php-autoloads.el ends here
--- .emacs.d/elpa/company-php-2.0.4/company-php-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "company-php" "2.0.4" "company completion source for php" '((cl-lib "0.5") (ac-php-core "1") (company "0.9")) :commit "326721527b2f88df7227cdc41708912b2f9c8494" :keywords '("completion" "convenience" "intellisense") :url "https://github.com/xcwen/ac-php")
