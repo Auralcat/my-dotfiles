1b60fd9bb9f5d76bb14cf57f63319a26b22f839e
Auralcat <ashleyzt@hi2.in>
2018-06-08 01:31:14 -0300

    Upgrade packages, install cyberpunk-theme

---
+++ .emacs.d/elpa/ac-php-core-2.0.5/ac-php-core-autoloads.el
@@ -0,0 +1,27 @@
+;;; ac-php-core-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ac-php-core" "ac-php-core.el" (23322 1157
+;;;;;;  278885 328000))
+;;; Generated autoloads from ac-php-core.el
+
+(autoload 'ac-php-core-eldoc-setup "ac-php-core" "\
+Set up eldoc function and enable eldoc-mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-php-comm-tags-data.el" "ac-php-core-pkg.el")
+;;;;;;  (23322 1157 413235 265000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-php-core-autoloads.el ends here
---
+++ .emacs.d/elpa/cyberpunk-theme-1.20/cyberpunk-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cyberpunk-theme" "1.20" "Cyberpunk Color Theme" 'nil :commit "2f12f08489d88efdb3ff6ca9489ef819f0f3b428" :keywords '("color" "theme" "cyberpunk"))
