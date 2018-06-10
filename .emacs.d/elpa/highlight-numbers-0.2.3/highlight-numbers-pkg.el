d8e7d657f78abc352e9c33494e55c779b0f3d57f
Auralcat <ashleyzt@hi2.in>
2018-06-10 11:55:32 -0300

    Forgot to add the contents! 😅

---
+++ .emacs.d/elpa/highlight-numbers-0.2.3/highlight-numbers-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-numbers" "0.2.3" "Highlight numbers in source code" '((emacs "24") (parent-mode "2.0")) :commit "b7adef0286aaa5bca8e98a12d0ffed3a880e25aa" :url "https://github.com/Fanael/highlight-numbers")
