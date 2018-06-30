4f3d64a5f153ee4096cf219dd4d765ca4286c671
Auralcat <ashleyzt@hi2.in>
2018-06-30 13:28:51 -0300

    Learn about checkdoc

--- .emacs.d/elisp-practice-programs/get-divisors.el
+++ .emacs.d/elisp-practice-programs/get-divisors.el
@@ -1,11 +1,22 @@
+;;; get-divisors.el --- Get the list of divisors of a number
+
+;;; Commentary:
+;;
+
+;;; Code:
+
 (defun get-divisors (num)
-    "Returns the set of divisors of a given number"
+    "Return the set of divisors of a given number.
+Argument NUM: A number."
     (let ((count 2) (arr (list)))
     (while (< count (sqrt num))
         (when (eq (% num count) 0)
             (message "%s can be divided by %s" num count)
-            (setq arr (cons (list (/ num count) count) arr)))
+            (setq arr (append (list (/ num count) count) arr)))
         (setq count (1+ count)))
     (message "vector: %s" arr)))
 
-(get-divisors 5040)
+
+(provide 'get-divisors)
+
+;;; get-divisors.el ends here
