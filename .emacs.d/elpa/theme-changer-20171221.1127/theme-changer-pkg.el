34eeb755f5772aa1b2d467107aeab1f80644b160
Auralcat <ashleyzt@hi2.in>
2018-07-25 11:13:19 -0300

    Change abbrevs, add pomo sounds, replace theme-changer with circadian

--- .emacs.d/elpa/theme-changer-20171221.1127/theme-changer-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "theme-changer" "20171221.1127" "Sunrise/Sunset Theme Changer for Emacs" 'nil)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -323,11 +323,15 @@
  '(org-plantuml-jar-path "~/file-bouncer/plantuml.jar")
  '(org-pomodoro-audio-player "/usr/bin/mpv")
     '(org-pomodoro-finished-sound
-         "/home/lucas/.emacs.d/elpa/org-pomodoro-2.1.0/resources/Blip.ogg")
+         "/home/lucas/.emacs.d/pomodoro-sounds/Librem-5-power-on-by-Antonio-Paternina-Alvarez.ogg")
  '(org-pomodoro-finished-sound-p t)
  '(org-pomodoro-format "Focus!~%s")
- '(org-pomodoro-long-break-sound-p nil)
- '(org-pomodoro-short-break-sound-p nil)
+    '(org-pomodoro-long-break-sound
+         "/home/lucas/.emacs.d/pomodoro-sounds/Librem-5-phone-call-3-by-Nohumanconcept.ogg")
+ '(org-pomodoro-long-break-sound-p t)
+    '(org-pomodoro-short-break-sound
+         "/home/lucas/.emacs.d/pomodoro-sounds/Librem-5-email-notification-2-by-Pablo-Somonte.ogg")
+ '(org-pomodoro-short-break-sound-p t)
  '(org-pomodoro-start-sound-p nil)
  '(org-refile-targets (quote ((org-agenda-files :maxlevel . 3))))
  '(org-src-fontify-natively t)
@@ -340,7 +344,7 @@
  '(org-todo-keywords (quote ((sequence "TODO" "DONE"))))
     '(package-selected-packages
          (quote
-             (diminish alchemist sublimity writeroom-mode electric-operator jazz-theme birds-of-paradise-plus-theme highlight-indentation smartparens company-tern restart-emacs ob-restclient doom-themes intellij-theme github-modern-theme restclient helm-projectile ibuffer-projectile yasnippet-classic-snippets yasnippet-snippets vagrant-tramp ob-elixir company-prescient weechat web-mode web-beautify vagrant use-package twittering-mode theme-changer solarized-theme scss-mode sass-mode rvm rust-mode ruby-tools robe rich-minority request rainbow-mode rainbow-delimiters projectile powerline org-pomodoro org-bullets nyan-mode moe-theme mode-icons memoize makey magit keyfreq js2-mode htmlize highlight-sexp highlight-numbers helm-tramp flymake-phpcs flymake-php flymake-elixir flycheck-mix evil-surround evil-leader evil-anzu eshell-prompt-extras eruby-mode enh-ruby-mode engine-mode emojify emmet-mode electric-spacing editorconfig csv-mode company-web company-statistics company-php cl-generic camcorder autopair ace-jump-mode ac-html-bootstrap abyss-theme)))
+             (circadian diminish alchemist sublimity writeroom-mode electric-operator jazz-theme birds-of-paradise-plus-theme highlight-indentation smartparens company-tern restart-emacs ob-restclient doom-themes intellij-theme github-modern-theme restclient helm-projectile ibuffer-projectile yasnippet-classic-snippets yasnippet-snippets vagrant-tramp ob-elixir company-prescient weechat web-mode web-beautify vagrant use-package twittering-mode solarized-theme scss-mode sass-mode rvm rust-mode ruby-tools robe rich-minority request rainbow-mode rainbow-delimiters projectile powerline org-pomodoro org-bullets nyan-mode moe-theme mode-icons memoize makey magit keyfreq js2-mode htmlize highlight-sexp highlight-numbers helm-tramp flymake-phpcs flymake-php flymake-elixir flycheck-mix evil-surround evil-leader evil-anzu eshell-prompt-extras eruby-mode enh-ruby-mode engine-mode emojify emmet-mode electric-spacing editorconfig csv-mode company-web company-statistics company-php cl-generic camcorder autopair ace-jump-mode ac-html-bootstrap abyss-theme)))
  '(pdf-view-midnight-colors (quote ("#6a737d" . "#fffbdd")))
  '(persp-show-modestring (quote (quote header)))
  '(pos-tip-background-color "#073642")
