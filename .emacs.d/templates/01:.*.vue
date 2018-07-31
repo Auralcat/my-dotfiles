812276f107abacb1914edcc2079480c623c94b5d
Auralcat <ashleyzt@hi2.in>
2018-07-31 15:47:40 -0300

    Create Vue template

---
+++ .emacs.d/templates/01:.*.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      $0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
