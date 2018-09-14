feb60400917060da3e15e8031f520d6c918abe24
Auralcat <ashleyzt@hi2.in>
2018-09-14 09:53:25 -0300

    Upgrade packages

--- .emacs.d/elpa/ghub-20180911.823/ghub-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "ghub" "20180911.823" "Minuscule client libraries for Git forge APIs."
-  '((emacs "24.4")
-    (let-alist "1.0.5")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- .emacs.d/elpa/rjsx-mode-20180823.1645/rjsx-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rjsx-mode" "20180823.1645" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "c463a162a2557668f624c32dd5c3b58512e80b1a" :keywords '("languages") :authors '(("Felipe Ochoa" . "felipe@fov.space")) :maintainer '("Felipe Ochoa" . "felipe@fov.space") :url "https://github.com/felipeochoa/rjsx-mode/")
