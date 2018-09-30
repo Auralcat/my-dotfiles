506c949455e5dbc4036be5cc9bb168ee3bd07777
Auralcat <ashleyzt@hi2.in>
2018-09-30 14:11:04 -0300

    Install carbon-now-sh

---
+++ .emacs.d/elpa/carbon-now-sh-20180331.1735/carbon-now-sh-autoloads.el
@@ -0,0 +1,27 @@
+;;; carbon-now-sh-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "carbon-now-sh" "carbon-now-sh.el" (0 0 0 0))
+;;; Generated autoloads from carbon-now-sh.el
+
+(autoload 'carbon-now-sh "carbon-now-sh" "\
+Open current region in carbon.now.sh.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "carbon-now-sh" '("carbon-now-sh-baseurl")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; carbon-now-sh-autoloads.el ends here
---
+++ .emacs.d/elpa/carbon-now-sh-20180331.1735/carbon-now-sh-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "carbon-now-sh" "20180331.1735" "https://carbon.now.sh integration." '((emacs "24.4") (dash "2.12.0")) :commit "71dee6bc4f2a2cb02b9b7b5e643c4c92b880e6a4" :keywords '("convenience") :authors '(("Vitalii Elenhaupt")) :maintainer '("Vitalii Elenhaupt") :url "https://github.com/veelenga/carbon-now-sh.el")
