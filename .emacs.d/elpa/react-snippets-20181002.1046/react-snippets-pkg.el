b4a97f98657154ef3001c8f949fb2be6ff72e95b
Auralcat <ashleyzt@hi2.in>
2018-10-02 16:43:38 -0300

    Update React snippets

--- .emacs.d/elpa/react-snippets-20181002.1046/react-snippets-pkg.el
+++ .emacs.d/elpa/react-snippets-20181002.1046/react-snippets-pkg.el
@@ -1,4 +1,4 @@
-(define-package "react-snippets" "20170803.2250" "Yasnippets for React"
+(define-package "react-snippets" "20181002.1046" "Yasnippets for React"
   '((yasnippet "0.7.0")))
 ;; Local Variables:
 ;; no-byte-compile: t
