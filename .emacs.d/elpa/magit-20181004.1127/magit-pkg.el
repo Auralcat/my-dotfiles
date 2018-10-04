f3f8ce5c81ff11ee59f95fd99fed7df20f26ca32
Auralcat <ashleyzt@hi2.in>
2018-10-04 18:14:23 -0300

    Update Magit

--- .emacs.d/elpa/magit-20181004.1127/magit-pkg.el
+++ .emacs.d/elpa/magit-20181004.1127/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20180928.1153" "A Git porcelain inside Emacs."
+(define-package "magit" "20181004.1127" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180413")
