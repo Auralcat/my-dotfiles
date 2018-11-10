a62ea840897e2f368a072382643e22e943dcaeb1
Auralcat <ashleyzt@hi2.in>
2018-11-09 22:56:37 -0200

    ⬆️ update tern

---
+++ .emacs.d/elpa/tern-20181108.722/tern-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern" "20181108.722" "Tern-powered JavaScript integration" '((json "1.2") (cl-lib "0.5") (emacs "24")) :commit "40a0c74db3888b997a9115720ff91c399bb1b146" :authors '(("Marijn Haverbeke")) :maintainer '("Marijn Haverbeke") :url "http://ternjs.net/")
