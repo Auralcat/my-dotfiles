24ffcf46a329bcd3ade877a900d170a0e8410fba
Auralcat <ashleyzt@hi2.in>
2018-11-09 22:55:38 -0200

    ⬆️ update helm

--- .emacs.d/elpa/helm-20181109.1225/helm-pkg.el
+++ .emacs.d/elpa/helm-20181109.1225/helm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm" "20181029.623" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20181109.1225" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9.3")
     (popup "0.5.3")
