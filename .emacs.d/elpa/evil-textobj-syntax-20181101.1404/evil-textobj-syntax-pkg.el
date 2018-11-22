b5a560d2cead528e38639b6ce51e388ad031873b
Auralcat <miriamretka@tutanota.com>
2018-11-22 21:05:36 -0200

    :sparkles: Add evil-textobj-syntax

---
+++ .emacs.d/elpa/evil-textobj-syntax-20181101.1404/evil-textobj-syntax-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-textobj-syntax" "20181101.1404" "Provides syntax text objects." '((names "0.5") (emacs "24") (evil "0")) :commit "933752ff2ae22d1bbcda394bdeed5c575d90d1d8" :keywords '("evil" "syntax" "highlight" "text-object") :url "https://github.com/laishulu/evil-textobj-syntax")
