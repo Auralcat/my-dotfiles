8138a452284ce81d09a592f125a3f36f0233298d
Auralcat <miriamretka@tutanota.com>
2018-11-24 14:23:41 -0200

    :arrow_up: Update magit

--- .emacs.d/elpa/magit-20181122.1718/magit-pkg.el
+++ .emacs.d/elpa/magit-20181122.1718/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20181122.936" "A Git porcelain inside Emacs."
+(define-package "magit" "20181122.1718" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")
