1ed80942a4d018159b998c498245f3f6899cc4bd
Miriam Retka <miriamretka@Miriams-MacBook-Pro.local>
2018-12-09 19:29:28 -0200

    :sparkles: Increase font size in Mac

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
      ("melpa-stable" . "https://melpa-stable.milkbox.net/packages/")
      ;; Use it when you can't find what you want in other repos
      ("melpa" . "https://melpa.org/packages/")
-                            ))
+			    ))
 (package-initialize)
 
 ;; This replaces the old require-package.
@@ -96,15 +96,15 @@
  '(ensime-sem-high-faces
    (quote
     ((var :foreground "#000000" :underline
-          (:style wave :color "yellow"))
+	  (:style wave :color "yellow"))
      (val :foreground "#000000")
      (varField :foreground "#600e7a" :slant italic)
      (valField :foreground "#600e7a" :slant italic)
      (functionCall :foreground "#000000" :slant italic)
      (implicitConversion :underline
-                         (:color "#c0c0c0"))
+			 (:color "#c0c0c0"))
      (implicitParams :underline
-                     (:color "#c0c0c0"))
+		     (:color "#c0c0c0"))
      (operator :foreground "#000080")
      (param :foreground "#000000")
      (class :foreground "#20999d")
@@ -278,7 +278,7 @@
      ("\\` Emmet\\'" "emmet" xpm)
      ("\\`Vue\\'" "vue" xpm)
      ("\\` ?\\(?:ElDoc\\|Anzu\\|SP\\|Guide\\|PgLn\\|Undo-Tree\\|Ergo.*\\|,\\|Isearch\\|Ind\\)\\'" nil nil))))
- '(mode-icons-change-mode-name t)
+ '(mode-ons-change-mode-name t)
  '(mode-icons-generate-emoji-xpms t)
  '(mode-icons-generate-font-xpms t)
  '(mode-icons-mode t)
@@ -373,10 +373,10 @@
    (quote
     (:eval
      (if
-         (file-remote-p default-directory)
-         " Projectile"
+	 (file-remote-p default-directory)
+	 " Projectile"
        (format " [%s] "
-               (projectile-project-name))))))
+	       (projectile-project-name))))))
  '(reb-re-syntax (quote string))
  '(remember-data-file "~/file-bouncer/everything-bucket")
  '(save-place-mode t nil (saveplace))
@@ -388,8 +388,8 @@
  '(sml/mode-width
    (if
        (eq
-        (powerline-current-separator)
-        (quote arrow))
+	(powerline-current-separator)
+	(quote arrow))
        (quote right)
      (quote full)))
  '(sml/pos-id-separator
@@ -398,14 +398,14 @@
      (:propertize " " face powerline-active1)
      (:eval
       (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s"
-                            (powerline-current-separator)
-                            (car powerline-default-separator-dir)))
-                   (quote powerline-active1)
-                   (quote powerline-active2))))
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (car powerline-default-separator-dir)))
+		   (quote powerline-active1)
+		   (quote powerline-active2))))
      (:propertize " " face powerline-active2))))
  '(sml/pos-minor-modes-separator
    (quote
@@ -413,14 +413,14 @@
      (:propertize " " face powerline-active1)
      (:eval
       (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s"
-                            (powerline-current-separator)
-                            (cdr powerline-default-separator-dir)))
-                   (quote powerline-active1)
-                   (quote sml/global))))
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (cdr powerline-default-separator-dir)))
+		   (quote powerline-active1)
+		   (quote sml/global))))
      (:propertize " " face sml/global))))
  '(sml/pre-id-separator
    (quote
@@ -428,14 +428,14 @@
      (:propertize " " face sml/global)
      (:eval
       (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s"
-                            (powerline-current-separator)
-                            (car powerline-default-separator-dir)))
-                   (quote sml/global)
-                   (quote powerline-active1))))
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (car powerline-default-separator-dir)))
+		   (quote sml/global)
+		   (quote powerline-active1))))
      (:propertize " " face powerline-active1))))
  '(sml/pre-minor-modes-separator
    (quote
@@ -443,14 +443,14 @@
      (:propertize " " face powerline-active2)
      (:eval
       (propertize " "
-                  (quote display)
-                  (funcall
-                   (intern
-                    (format "powerline-%s-%s"
-                            (powerline-current-separator)
-                            (cdr powerline-default-separator-dir)))
-                   (quote powerline-active2)
-                   (quote powerline-active1))))
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (cdr powerline-default-separator-dir)))
+		   (quote powerline-active2)
+		   (quote powerline-active1))))
      (:propertize " " face powerline-active1))))
  '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes)))
  '(smtpmail-smtp-server "smtp.yandex.com")
