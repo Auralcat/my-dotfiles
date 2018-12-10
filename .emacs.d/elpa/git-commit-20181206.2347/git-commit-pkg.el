ca164c4a1db570ab6673bfc41b1640ef0917b232
Miriam Retka <miriamretka@Miriams-MacBook-Pro.local>
2018-12-09 23:50:47 -0200

    :arrow_up: Update magit

---
+++ .emacs.d/elpa/git-commit-20181206.2347/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20181206.2347" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "9175e95c606ccf5f204da5f3f23bc8d9c2875a53" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
