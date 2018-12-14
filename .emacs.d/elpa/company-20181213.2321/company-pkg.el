d6936c385e7d0501c4e34c034851e6fe19192ddc
Auralcat <ashleyzt@hi2.in>
2018-12-14 10:07:46 -0200

    :arrow_up: Update company

--- .emacs.d/elpa/company-20181213.2321/company-pkg.el
+++ .emacs.d/elpa/company-20181213.2321/company-pkg.el
@@ -1,4 +1,4 @@
-(define-package "company" "20181212.2244" "Modular text completion framework"
+(define-package "company" "20181213.2321" "Modular text completion framework"
   '((emacs "24.3"))
   :keywords
   '("abbrev" "convenience" "matching")
