b31af4f46ac5111773d9132c1224d84f14172a78
Auralcat <ashleyzt@hi2.in>
2018-12-19 10:37:11 -0200

    :arrow_up: Update magit

---
+++ .emacs.d/elpa/git-commit-20181218.1941/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20181218.1941" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "1618c6ee6b482a22af928e83ea0ca8c86f24a7d8" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
