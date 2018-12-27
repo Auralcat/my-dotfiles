ccc1e5a73302137c3bbff6d701723b03da890883
Auralcat <ashleyzt@hi2.in>
2018-12-27 11:11:10 -0200

    :arrow_up: Update editorconfig

--- .emacs.d/elpa/editorconfig-20181225.249/editorconfig-pkg.el
+++ .emacs.d/elpa/editorconfig-20181225.249/editorconfig-pkg.el
@@ -1,4 +1,4 @@
-(define-package "editorconfig" "20181217.724" "EditorConfig Emacs Plugin"
+(define-package "editorconfig" "20181225.249" "EditorConfig Emacs Plugin"
   '((cl-lib "0.5"))
   :authors
   '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
