e6ff0ef86268eafbf1ddcfb70c519c88023f80ac
Auralcat <ashleyzt@hi2.in>
2018-12-28 15:46:59 -0200

    :arrow_up: Update evil-textobj-syntax

---
+++ .emacs.d/elpa/evil-textobj-syntax-20181210.1213/evil-textobj-syntax-autoloads.el
@@ -0,0 +1,25 @@
+;;; evil-textobj-syntax-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "evil-textobj-syntax" "evil-textobj-syntax.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from evil-textobj-syntax.el
+ (autoload 'evil-a-syntax "evil-textobj-syntax" nil t)
+ (autoload 'evil-i-syntax "evil-textobj-syntax" nil t)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "evil-textobj-syntax" '("evil-textobj-syntax-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; evil-textobj-syntax-autoloads.el ends here
---
+++ .emacs.d/elpa/evil-textobj-syntax-20181210.1213/evil-textobj-syntax-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-textobj-syntax" "20181210.1213" "Provides syntax text objects." '((names "0.5") (emacs "24") (evil "0")) :commit "2d9ba8c75c754b409aea7469f46a5cfa52a872f3" :keywords '("evil" "syntax" "highlight" "text-object") :url "https://github.com/laishulu/evil-textobj-syntax")
