77be916efa240dea7106b074214c28f13453ebfe
Auralcat <ashleyzt@hi2.in>
2019-01-01 10:24:16 -0200

    :arrow_up: Update js completion

--- .emacs.d/elpa/ac-js2-20190101.933/ac-js2-pkg.el
+++ .emacs.d/elpa/ac-js2-20190101.933/ac-js2-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ac-js2" "20140906.1142" "Auto-complete source for Js2-mode, with navigation"
+(define-package "ac-js2" "20190101.933" "Auto-complete source for Js2-mode, with navigation"
   '((js2-mode "20090723")
     (skewer-mode "1.4"))
   :authors
