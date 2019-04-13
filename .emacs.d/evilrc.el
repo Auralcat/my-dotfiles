9387263b36c4cb656eeb8a40bfb03b0db1bc642b
Auralcat <ashleyzt@hi2.in>
2019-04-12 21:19:33 -0300

    :sparkles: Add Eyebrowse to the configurations
    Along with it, I mapped the functions to Evil-leader.

--- .emacs.d/evilrc.el
+++ .emacs.d/evilrc.el
@@ -128,4 +128,23 @@
     "smg" 'slack-group-select
     "smc" 'slack-channel-select
     ;; TODO: map this to org-mode locally.
-    "t" 'my-org-remote-todo)
+    "t" 'my-org-remote-todo
+    ;; Eyebrowse configuration
+    "w\""  'eyebrowse-close-window-config
+    "w\'"  'eyebrowse-last-window-config
+    "w,"   'eyebrowse-rename-window-config
+    "w."   'eyebrowse-switch-to-window-config
+    "w0"   'eyebrowse-switch-to-window-config-0
+    "w1"   'eyebrowse-switch-to-window-config-1
+    "w2"   'eyebrowse-switch-to-window-config-2
+    "w3"   'eyebrowse-switch-to-window-config-3
+    "w4"   'eyebrowse-switch-to-window-config-4
+    "w5"   'eyebrowse-switch-to-window-config-5
+    "w6"   'eyebrowse-switch-to-window-config-6
+    "w7"   'eyebrowse-switch-to-window-config-7
+    "w8"   'eyebrowse-switch-to-window-config-8
+    "w9"   'eyebrowse-switch-to-window-config-9
+    "w<"   'eyebrowse-prev-window-config
+    "w>"   'eyebrowse-next-window-config
+    "wc"   'eyebrowse-create-window-config
+)
