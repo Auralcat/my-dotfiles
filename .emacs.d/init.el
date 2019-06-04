a11f1160b9d9bb0501ea64b41401cb5a5766a236
Auralcat <ashleyzt@hi2.in>
2019-06-03 22:34:46 -0300

    ✍ Adjust Emacs for writing
    This commit adds a couple tweaks for writing modes and sets org-src
    blocks with a fixed pitch font.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -410,4 +410,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "Monaco"))))
+ '(variable-pitch ((t (:weight normal :height 160 :family "Gill Sans")))))
+(put 'dired-find-alternate-file 'disabled nil)
