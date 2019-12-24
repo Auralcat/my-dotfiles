6eef7cb9c383c4b0bcf3d3207a90bd9cc56a0950
Auralcat <ashleyzt@hi2.in>
2019-12-24 15:44:33 -0300

    Update reading specs
    Helvetica turned out to be a better font for reading.

--- .emacs.d/reading-specs-theme.el
+++ .emacs.d/reading-specs-theme.el
@@ -4,11 +4,21 @@
    Created 2019-06-11.")
 
 (custom-theme-set-faces
- 'reading-specs
- '(variable-pitch ((t (:family "IBM Plex Sans" :height 140))))
- '(default ((t (:family "Inconsolata"))))
- '(org-date ((t (:inherit fixed-pitch))))
- '(org-table ((t (:inherit fixed-pitch))))
- '(org-src ((t (:inherit fixed-pitch)))))
+  'reading-specs
+  '(variable-pitch ((t (:weight normal :height 160 :family "Helvetica"))))
+  '(default ((t (:family "Inconsolata" :height 120))))
+  '(org-date ((t (:inherit fixed-pitch))))
+  '(org-table ((t (:inherit fixed-pitch))))
+  '(org-src ((t (:inherit fixed-pitch))))
+
+  '(outline-1  ((t  (:font-weight 600 :height  220))))
+  '(outline-2  ((t  (:height  200 :inherit outline-1))))
+  '(outline-3  ((t  (:height  180 :inherit outline-1))))
+  '(outline-4  ((t  (:height  160 :inherit outline-1))))
+
+  '(org-level-1  ((t  (:inherit outline-1))))
+  '(org-level-2  ((t  (:inherit outline-2))))
+  '(org-level-3  ((t  (:inherit outline-3))))
+  '(org-level-4  ((t  (:inherit outline-4)))))
 
 (provide-theme 'reading-specs)
